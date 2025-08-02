8e42818f287f6539131f4f261daa6f1bfb5f638c
Paperang <Paperang@DESKTOP-7HQO7ME>
2025-08-02 09:30:01 +0800

    添加项目文件。

---
+++ leetCode_20/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+namespace leetCode_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Solution s = new Solution();
+            if (s.IsValid("{}[]"))
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            Stack stack = new Stack();
+
+
+            int i = 0;
+            int length = s.Length;
+
+            while (i < length)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    stack.Push(s[i]);
+                }
+                else if (stack.Count != 0)
+                {
+                    if (s[i] == ')' && (char)stack.Pop() != '(') return false;
+                    else if (s[i] == '}' && (char)stack.Pop() != '{') return false;
+                    else if (s[i] == ']' && (char)stack.Pop() != '[') return false;
+                }
+                i++;
+            }
+            if (stack.Count != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetCode_26/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace leetCode_26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] nums = { 1, 1, 2, 2, 3, 4, 4, 4, 5, 5 };
+            Solution s = new Solution();
+            int k = s.RemoveDuplicates(ref nums);
+            int[] arr = new int[k];
+            for(int i = 0; i < k; i++)
+            {
+                arr[i] = nums[i];
+            }
+            nums = arr;
+            Console.WriteLine($"长度：{k}, 数组：{String.Join(" ",nums)}");
+        }
+    }
+    public class Solution
+    {
+        public int RemoveDuplicates(ref int[] nums)
+        {
+            int k = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[k])
+                {
+                    nums[++k] = nums[i];
+                }
+            }
+            k += 1;
+            return k;
+        }
+    }
+}
