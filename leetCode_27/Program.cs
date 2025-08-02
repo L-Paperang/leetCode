a2941c46448ca4b3262034d84c37560c0c2b9510
L-Paperang <2683132289@qq.com>
2025-08-02 10:30:53 +0800

    Add files via upload

---
+++ leetCode_27/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace leetCode_27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] nums = { 3, 2, 2, 3 };
+            int val = 3;
+            int result = Solution.RemoveElement(nums, val);
+            Console.WriteLine(String.Join(", ", nums.Take(result)));
+        }
+    }
+    public static class Solution
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[k++] = nums[i];
+                }
+            }
+            return k;
+        }
+    }
+}
